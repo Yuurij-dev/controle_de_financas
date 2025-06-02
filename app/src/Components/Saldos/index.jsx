bfda441de78e3e1d98682e83c0eeec87743e7a68
Yuurij-dev <yurijesus.dev@gmail.com>
2025-06-02 16:55:47 -0300

    Adicionando formatação de reais 4

--- app/src/Components/Saldos/index.jsx
+++ app/src/Components/Saldos/index.jsx
@@ -3,56 +3,48 @@ import './styles.css'
 import { useState } from 'react'
 
 function Saldo({valor}) {
-  const valorFormated = valor.toLocaleString('pt-BR',{
+  const valorFormated = valor.toLocaleString('pt-br',{
     minimumFractionDigits: 2,
     maximumFractionDigits: 2
   })
-  
-  const valorFormatadoComPonto = valorFormated.replace(',', '.');
-
   return (
     <div className='card'>
         <h3>Saldo Total</h3>
         
         <div className='flex gap-2 items-center'>
-          <span className={valor < 0 ? 'text-red-500' : 'text-green-500'}>R$ {valorFormatadoComPonto}</span>
+          <span className={valor < 0 ? 'text-red-500' : 'text-green-500'}>R$ {valorFormated}</span>
         </div>
     </div>
   )
 }
 
 function Receitas({valor}) {
-  const valorFormated = valor.toLocaleString('pt-BR',{
+  const valorFormated = valor.toLocaleString('pt-br',{
     minimumFractionDigits: 2,
     maximumFractionDigits: 2
   })
-  
-const valorFormatadoComPonto = valorFormated.replace(',', '.');
   return (
     <div className='card'>
         <h3>Receitas</h3>
 
         <div className='flex gap-2 items-center'>
-          <span className={valor < 0 ? 'text-red-500' : 'text-green-500'}>R$ {valorFormatadoComPonto}</span>
+          <span className={valor < 0 ? 'text-red-500' : 'text-green-500'}>R$ {valorFormated}</span>
         </div>
     </div>
   )
 }
 
 function Despesa({valor}) {
-  const valorFormated = valor.toLocaleString('pt-BR',{
+  const valorFormated = valor.toLocaleString('pt-br',{
     minimumFractionDigits: 2,
     maximumFractionDigits: 2
   })
-  
-const valorFormatadoComPonto = valorFormated.replace(',', '.');
-
   return (
     <div className='card card-last'>
         <h3>Despesas</h3>
 
         <div className='flex gap-2 items-center'>
-          <span className='text-red-500' >R$ {valorFormatadoComPonto}</span>         
+          <span className='text-red-500' >R$ {valorFormated}</span>         
         </div>
     </div>
   )
