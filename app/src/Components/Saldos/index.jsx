b29df0032fd4faa42f9ff3e745b1c477faea94a4
Yuurij-dev <yurijesus.dev@gmail.com>
2025-06-04 16:57:53 -0300

    fix: format currency usuin Intl.NumberFomart

--- app/src/Components/Saldos/index.jsx
+++ app/src/Components/Saldos/index.jsx
@@ -3,49 +3,49 @@ import './styles.css'
 import { useState } from 'react'
 
 function Saldo({valor}) {
-  const valorFormated = valor.toLocaleString('pt-BR',{
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2
-  })
+  const valorFormated = new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(valor)
   
   return (
     <div className='card'>
         <h3>Saldo Total</h3>
         
         <div className='flex gap-2 items-center'>
-          <span className={valor < 0 ? 'text-red-500' : 'text-green-500'}>R$ {valorFormated}</span>
+          <span className={valor < 0 ? 'text-red-500' : 'text-green-500'}>{valorFormated}</span>
         </div>
     </div>
   )
 }
 
 function Receitas({valor}) {
-  const valorFormated = valor.toLocaleString('pt-BR',{
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2
-  })
+  const valorFormated = new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(valor)
   return (
     <div className='card'>
         <h3>Receitas</h3>
 
         <div className='flex gap-2 items-center'>
-          <span className={valor < 0 ? 'text-red-500' : 'text-green-500'}>R$ {valorFormated}</span>
+          <span className={valor < 0 ? 'text-red-500' : 'text-green-500'}>{valorFormated}</span>
         </div>
     </div>
   )
 }
 
 function Despesa({valor}) {
-  const valorFormated = valor.toLocaleString('pt-BR',{
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2
-  })
+  const valorFormated = new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(valor)
   return (
     <div className='card card-last'>
         <h3>Despesas</h3>
 
         <div className='flex gap-2 items-center'>
-          <span className='text-red-500' >R$ {valorFormated}</span>         
+          <span className='text-red-500' >{valorFormated}</span>         
         </div>
     </div>
   )
